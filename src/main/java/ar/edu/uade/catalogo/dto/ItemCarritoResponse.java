3439e45d4792660bedb1477e042c558bc7bf0bc7
Nicolas Ferrante <nicoferra2005@gmail.com>
2026-09-07 22:14:41 -0300

    Update ItemCarritoResponse.java

--- src/main/java/ar/edu/uade/catalogo/dto/ItemCarritoResponse.java
+++ src/main/java/ar/edu/uade/catalogo/dto/ItemCarritoResponse.java
@@ -1,5 +1,5 @@
 package ar.edu.uade.catalogo.dto;
 import java.math.BigDecimal;
-public record ItemCarritoResponse(Long id, LOng productoId, String productoNombre, BigDecimal precioUnitario,int cantidad, BigDecimal subtotal){
+public record ItemCarritoResponse(Long id, Long productoId, String productoNombre, BigDecimal precioUnitario,int cantidad, BigDecimal subtotal){
   
 }
