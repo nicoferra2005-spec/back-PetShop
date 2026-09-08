a3cb4d3a37c3b8c76293e466116b5eea3bfd04e2
Nicolas Ferrante <nicoferra2005@gmail.com>
2026-09-07 22:08:02 -0300

    Add findByUsuarioId method to CarritoRepository
    Added a method to find Carrito by usuarioId.

--- src/main/java/ar/edu/uade/catalogo/repository/CarritoRepository.java
+++ src/main/java/ar/edu/uade/catalogo/repository/CarritoRepository.java
@@ -1,4 +1,7 @@
 package ar.edu.uade.catalogo.repository;
 import ar.edu.uade.catalogo.model.Carrito;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface CarritoRepository extends JpaRepository<Carrito, Long> { }
+import java.util.Optional;
+public interface CarritoRepository extends JpaRepository<Carrito, Long> {
+  Optional<Carrito> findByUsuarioId(Long usuarioId);
+}
