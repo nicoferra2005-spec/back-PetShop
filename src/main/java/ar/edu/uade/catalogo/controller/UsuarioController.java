7ea86c90ea8e00b53414604b094e0d198b141a8c
Nicolas Ferrante <nicoferra2005@gmail.com>
2026-09-20 22:29:38 -0300

    Remove user registration and login endpoints
    Removed registration and login endpoints from UsuarioController.

--- src/main/java/ar/edu/uade/catalogo/controller/UsuarioController.java
+++ src/main/java/ar/edu/uade/catalogo/controller/UsuarioController.java
@@ -2,21 +2,15 @@ package ar.edu.uade.catalogo.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import ar.edu.uade.catalogo.dto.LoginRequest;
-import ar.edu.uade.catalogo.dto.RegisterRequest;
 import ar.edu.uade.catalogo.dto.UsuarioResponseDTO;
 import ar.edu.uade.catalogo.service.UsuarioService;
-import jakarta.validation.Valid;
 
 // http://localhost:8080/api/usuarios
 @RestController
@@ -48,19 +42,4 @@ public class UsuarioController {
         UsuarioResponseDTO usuario = usuarioService.findUsuarioByCorreo(correo);
         return ResponseEntity.ok().body(usuario);
     }
-
-    // post http://localhost:8080/api/usuarios/registro
-    @PostMapping("/registro")
-    public ResponseEntity<UsuarioResponseDTO> registrarUsuario(@Valid @RequestBody RegisterRequest registerRequest) {
-        UsuarioResponseDTO usuarioRegistrado = usuarioService.registrarUsuario(registerRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRegistrado);
-    }
-
-    // post http://localhost:8080/api/usuarios/login
-    @PostMapping("/login")
-    public ResponseEntity<UsuarioResponseDTO> login(@Valid @RequestBody LoginRequest loginRequest) {
-        UsuarioResponseDTO usuarioLogueado = usuarioService.login(loginRequest);
-        return ResponseEntity.ok().body(usuarioLogueado);
-    }
-
 }
